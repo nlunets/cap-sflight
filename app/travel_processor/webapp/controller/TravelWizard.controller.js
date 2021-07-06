4cbf4f222929d7029280811e807c1713e55fed41
Nicolas Lunet <nicolas.lunet@sap.com>
2021-07-06 22:53:37 +0200

    use wizard instead of OP

---
+++ app/travel_processor/webapp/controller/TravelWizard.controller.js
@@ -0,0 +1,6 @@
+sap.ui.define(["sap/fe/core/PageController"], function(PageController) {
+    "use strict";
+
+    return PageController.extend("sap.fe.cap.travel.controller.TravelWizard", {
+    });
+});
